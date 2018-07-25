34481217359b780b706151597811c3ae5966d008
miguelMontiel <miguelmontielgalicia@hotmail.com>
2018-07-25 13:46:13 -0500

    Add files via upload

--- src/Post.java
+++ src/Post.java
@@ -12,7 +12,7 @@ import org.json.JSONObject;
 
 public class Post 
 {	
-	public Map<String, String> sendPost(String pregunta) throws Exception
+	public Map<String, String> searchQuestion(String pregunta) throws Exception
 	{	
 		URL url = new URL("https://cognitiveassistant.in.edst.ibm.com/apiserver/api/v1/ask/direct/custom");
 		HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
@@ -50,21 +50,56 @@ public class Post
 		JSONArray jsonarray = jsonobject.getJSONArray("answers");
 		String question = null;
 		String answer = null;
-		double confidence = 0;
 		
 		Map<String, String> questionAnswer = new HashMap<String, String>();
 		
 		for(int i = 0; i < jsonarray.length(); i++)
 		{
-			//confidence = (double) jsonarray.getJSONObject(i).get("confidence") * 100;
 			question = jsonarray.getJSONObject(i).getString("question");
 			answer = jsonarray.getJSONObject(i).getString("answer");
 			
 			questionAnswer.put(question, answer);
-			
-			//System.out.println(questionAnswer.get(question));
 		}
-		
 		return questionAnswer;
+	}
+	
+	public void postQuestion(String pregunta, String mail) throws Exception
+	{	
+		URL url = new URL("https://cognitiveassistant.in.edst.ibm.com:443/apiserver/api/v1/postQuestion");
+		HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
+		con.setRequestMethod("POST");
+		con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+		
+		Map<String, String> urlParameters = new HashMap<>();
+		urlParameters.put("apiToken", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.IjRlN2ZjZTgzLThlN2EtNGYxYy1hYjk0LTE3N2E2YWIzODE4NCI.7I28yN8OOhv_6kQYNZ62BnqWuWGBhkxZAMFj81oMUOOAr35k3PYJLapC5-SJQ5rbfP591k_I9TG_5AM3TeMJbQ");
+		urlParameters.put("semanticQuestion", pregunta);
+		urlParameters.put("projectid", "2215");
+		urlParameters.put("userid", mail);
+		urlParameters.put("source", "AgentAssist");
+		urlParameters.put("curatedLang", "es");
+		urlParameters.put("username", mail);
+		urlParameters.put("error_code", "");
+		
+		con.setDoOutput(true);
+		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+		
+		wr.writeBytes(ParameterStringBuilder.getParamsString(urlParameters));
+		wr.flush();
+		wr.close();
+		
+		System.out.println("\nSending 'POST' request to URL: " + url);
+		System.out.println("Post parameters: " + urlParameters);
+		
+		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+		
+		while((inputLine = in.readLine())!= null)
+		{
+			response.append(inputLine);
+		}
+		in.close();
+		
+		System.out.println(response.toString());
 	}
 }
--- src/Window.java
+++ src/Window.java
@@ -12,7 +12,9 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
+import javax.swing.JTextPane;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.border.LineBorder;
 
@@ -20,16 +22,26 @@ public class Window extends JFrame implements ActionListener
 {
 	private static final long serialVersionUID = 1L;
 	private JFrame jframe = new JFrame();
-	private JPanel jpanelInput = new JPanel();
-	private JPanel jpanelOutput = new JPanel();
-	private JScrollPane jscrollpane = new JScrollPane(jpanelOutput, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-	private BorderLayout borderlayout = new BorderLayout();
-
-	private JLabel jlabel = new JLabel();
-	private JLabel jlabel2 = new JLabel();
 	
-	private JTextField jtextfield = new JTextField(30);
-	private JButton jbutton = new JButton("Enviar");
+	private JTabbedPane jtabbedpane = new JTabbedPane();
+	
+	private JPanel jpanelConsulta = new JPanel();
+	private JPanel jpanelInput = new JPanel();
+	private JPanel jpanelOutputButton = new JPanel();
+	private JPanel jpanelOutputLabel = new JPanel();
+	
+	private JPanel jpanelSubir = new JPanel();
+	
+	private JScrollPane jscrollpane = new JScrollPane(jpanelOutputButton, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+	private JScrollPane jscrollpane2 = new JScrollPane(jpanelOutputLabel, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+	private JTextPane jtextpane = new JTextPane();
+	
+	private JTextField jtextfieldSearch = new JTextField(40);
+	private JTextField jtextfieldMail = new JTextField(30);
+	private JTextField jtextfieldPost = new JTextField(40);
+	private JButton jbuttonSearch = new JButton("Enviar");
+	private JButton jbuttonPost = new JButton("Subir pregunta");
 	
 	private Cursor handCursor = new Cursor(Cursor.HAND_CURSOR);
 	private Cursor textCursor = new Cursor(Cursor.TEXT_CURSOR);
@@ -38,69 +50,106 @@ public class Window extends JFrame implements ActionListener
 	
 	Window()
 	{		
-		jframe.setLayout(borderlayout);
+		jpanelConsulta.setLayout(new BorderLayout());
 		
-		jbutton.setCursor(handCursor);
-		jlabel.setCursor(textCursor);
-		jtextfield.setCursor(textCursor);
+		jbuttonSearch.setCursor(handCursor);
+		jbuttonPost.setCursor(handCursor);
+		jtextfieldSearch.setCursor(textCursor);
+		jtextfieldPost.setCursor(textCursor);
+		jtextpane.setCursor(textCursor);
 		
-		jlabel.setVisible(false);
+		jtextpane.setContentType("text/html");
+		jtextpane.setEditable(false);
 		
-		jbutton.addActionListener(this);
+		jbuttonSearch.addActionListener(this);
+		jbuttonPost.addActionListener(this);
 		
-		jpanelOutput.setBorder(LineBorder.createBlackLineBorder());
-		jpanelOutput.setLayout(new BoxLayout(jpanelOutput, BoxLayout.Y_AXIS));
+		jpanelOutputButton.setBorder(LineBorder.createBlackLineBorder());
+		jpanelOutputLabel.setBorder(LineBorder.createBlackLineBorder());
+		jpanelOutputButton.setLayout(new BoxLayout(jpanelOutputButton, BoxLayout.Y_AXIS));
+
+		jpanelInput.add(jtextfieldSearch);
+		jpanelInput.add(jbuttonSearch);
 		
-		jpanelInput.add(jtextfield);
-		jpanelInput.add(jbutton);
-		jpanelInput.add(jlabel);
+		jpanelConsulta.add(jscrollpane, BorderLayout.NORTH);
+		jpanelConsulta.add(jscrollpane2, BorderLayout.CENTER);
+		jpanelConsulta.add(jpanelInput, BorderLayout.SOUTH);
 		
-		jframe.add(jscrollpane);
-		jframe.add(jpanelInput, BorderLayout.SOUTH);
+		jpanelSubir.add(jtextfieldMail);
+		jpanelSubir.add(jtextfieldPost);
+		jpanelSubir.add(jbuttonPost);
+		
+		jtabbedpane.addTab("Buscar", jpanelConsulta);
+		jtabbedpane.addTab("Subir", jpanelSubir);
+		
+		jframe.add(jtabbedpane);
 	}
 	
 	public void showGUI()
 	{
 		jframe.setTitle("HTTP Request DEMO");
-		jframe.setSize(720, 960);
+		jframe.setSize(768, 960);
 		jframe.setVisible(true);
 		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
-	public void sendQuestion() throws Exception
+	public void searchQuestion() throws Exception
 	{			
-		String pregunta = jtextfield.getText();
-		Map<String, String> respuesta = post.sendPost(pregunta);
+		jpanelOutputButton.removeAll();
+		
+		String pregunta = jtextfieldSearch.getText();
+		Map<String, String> respuesta = post.searchQuestion(pregunta);
+		jpanelOutputButton.add(new JLabel("<html><h1>" + pregunta + "</h1><html>"));
+		
 		for(String key : respuesta.keySet())
 		{
 			System.out.println(key);
-			jpanelOutput.add(new JButton(new AbstractAction(key)
+			jpanelOutputButton.add(new JButton(new AbstractAction(key)
 			{
 				private static final long serialVersionUID = 1L;
 				public void actionPerformed(ActionEvent e)
 				{	
 					System.out.println(respuesta.get(key));
-					jlabel2.setText("<html><h1>" + key + "</h1><div width = 500>" + respuesta.get(key)+ "</div><br></html>");
-					jpanelOutput.add(jlabel2);
-					
-					jframe.revalidate();
-					jframe.repaint();
+					jtextpane.setText("<html><h1>" + key + "</h1><div>" + respuesta.get(key)+ "</div><br></html>");
+					jpanelOutputLabel.add(jtextpane);
 				}
 			}));
-		}		
-		jtextfield.setText("");
-		jtextfield.requestFocus();
+		}
+		jtextfieldSearch.setText("");
+		jtextfieldSearch.requestFocus();
+		
+		jframe.revalidate();
+		jframe.repaint();
+	}
+	
+	public void postQuestion()
+	{
+		String mail = jtextfieldMail.getText();
+		String pregunta = jtextfieldPost.getText();
+		jtextfieldPost.setText("");
+		
+		try
+		{
+			post.postQuestion(pregunta, mail);
+			jpanelSubir.add(new JLabel("Se subió tu pregunta: '" + pregunta + "'"));
+			
+			jframe.revalidate();
+			jframe.repaint();
+		}
+		catch(Exception ex) { ex.printStackTrace(); }
 	}
 
 	public void actionPerformed(ActionEvent e) 
 	{
-		if(e.getSource() == jbutton && !(jtextfield.getText().isEmpty()))
+		if(e.getSource() == jbuttonSearch && !(jtextfieldSearch.getText().isEmpty()))
 		{
-			jlabel.setVisible(true);
-			try 
-			{ 
-				sendQuestion(); 
-			} 
+			try { searchQuestion(); }
+			catch (Exception ex) { ex.printStackTrace(); }
+		}
+		
+		if(e.getSource() == jbuttonPost && !(jtextfieldPost.getText().isEmpty()))
+		{
+			try { postQuestion(); }
 			catch (Exception ex) { ex.printStackTrace(); }
 		}
 		else { JOptionPane.showMessageDialog(jframe, "Ingresa una pregunta valida", "ERROR", JOptionPane.ERROR_MESSAGE); }
